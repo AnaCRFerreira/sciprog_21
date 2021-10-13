fae888d0d1fa1a7950863bd43f5dde3666dbcc5a
U-TYNDALL\ana.ferreira <ana.ferreira@LAPCC483A621D3D.tyndall.ie>
2021-10-13 13:00:50 +0100

    Calculate the area

--- practical04/trapezodial_v2.c
+++ practical04/trapezodial_v2.c
@@ -20,7 +20,22 @@ int main(){
 	
 	}
 
+	//Find the are by the trapezodial rule
+	float area;
+	area=TanArr[0]+TanArr[N];
+	for(i=1; i<N; i++){
+		area=area+2.0*TanArr[i];
+	}
 
+	//Multiply area by (pi/3)/2N after converting it to radians
+	float mult_rad=degtorad((60.0-0.0)/(2.0*N));
+	area=mult_rad*area;
+
+	//Approximated result
+	printf("\n Trapezodial result: %f\n", area);
+
+	//Actual result(ln(2))
+	printf("Real result:%f\n", log(2.0));
 
 return 0;
 }
