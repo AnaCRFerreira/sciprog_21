923a73b3e68f73c38942cd5f16e7f85a516afcb3
U-TYNDALL\ana.ferreira <ana.ferreira@LAPCC483A621D3D.tyndall.ie>
2021-10-13 22:41:17 +0100

    Add new function

--- practical04/trapezodial_v2.c
+++ practical04/trapezodial_v2.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include<math.h>
 
-//declare function
+//declare subroutines
 float degtorad(float degang);
+float traprule(int N, float TanArr[N+1]); 
 
 int main(){
 	//Define variables
@@ -17,19 +18,15 @@ int main(){
 		rad=degtorad(deg);
 		TanArr[i]=tan(rad);
 		printf("TanArr[%d]=%f (f(x) at x=%d)\n", i, TanArr[i], i);
+
 	
 	}
 
 	//Find the are by the trapezodial rule
 	float area;
-	area=TanArr[0]+TanArr[N];
-	for(i=1; i<N; i++){
-		area=area+2.0*TanArr[i];
-	}
+	area=traprule(N, TanArr);
 
-	//Multiply area by (pi/3)/2N after converting it to radians
-	float mult_rad=degtorad((60.0-0.0)/(2.0*N));
-	area=mult_rad*area;
+
 
 	//Approximated result
 	printf("\n Trapezodial result: %f\n", area);
@@ -45,4 +42,20 @@ return 0;
 float degtorad(float degang){
 
 	return((M_PI * degang)/180.0);
+}
+
+
+float traprule(int N, float TanArr[N+1]){
+	int i;
+	float area;
+	area=TanArr[0]+TanArr[N];
+        for(i=1; i<N; i++){
+                area=area+2.0*TanArr[i];
+        }
+
+        //Multiply area by (pi/3)/2N after converting it to radians
+        float mult_rad=degtorad((60.0-0.0)/(2.0*N));
+        area=mult_rad*area;
+	return area;
+
 }
