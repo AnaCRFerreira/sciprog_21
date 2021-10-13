890eecd0fbfdb7d65dc343d5d5373adfe0f29508
U-TYNDALL\ana.ferreira <ana.ferreira@LAPCC483A621D3D.tyndall.ie>
2021-10-13 21:20:13 +0100

    printing script complete and tested

---
+++ practical03/printing.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void){
+
+	//declare two integers and assign values
+	int i1=10, i2=200;
+
+	//declare two floats and assign values
+	float fl1=1.11, fl2=2.2222;
+
+	//print the floats (floats with 4 decimal places)
+	printf("Two integers are %d %d and two floats are %.4f %.4f \n", i1, i2,
+fl1, fl2);
+
+	return 0;
+}
+
