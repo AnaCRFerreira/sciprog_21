890bb93ea4c4556391c722f5c4053be5540e4b5c
U-TYNDALL\ana.ferreira <ana.ferreira@LAPCC483A621D3D.tyndall.ie>
2021-10-19 10:20:17 +0100

    print adress

--- practical05/scanf.c
+++ practical05/scanf.c
@@ -5,7 +5,21 @@ int main(void) {
 	int i; double a;
 
 	// Enter information from user
-	printf("Enter an int and double\n");
-	
+	printf("Enter an int and double, separately by (,)\n");
+		
 	scanf("%d %lf",&i,&a);
+	
+	printf("You have entered %d, and %lf\n", i, a);
+
+	int *pointer_to_i = &i;
+	//Print the r-value of i
+	printf("The value i is %d\n",i);
+
+	//Print the r-value of i using pointers 
+	printf("The value of i is also %d\n",  *pointer_to_i);
+
+	//Print the l-value of i
+	printf("The address of i is %d\n", &i);
+
+	return 0;
 }
