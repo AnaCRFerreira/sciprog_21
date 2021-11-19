157f1528459191cc12a6eceb9cbb45196d5fcfa7
U-TYNDALL\ana.ferreira <ana.ferreira@LAPCC483A621D3D.tyndall.ie>
2021-11-19 11:42:45 +0000

    Edit comments and identation

--- practical02/Sum.c
+++ practical02/Sum.c
@@ -2,34 +2,36 @@
 
 
 int main(void) {
-/* Declare variables */
-   int i;
-   float sum1, sum2, diff;
+	/* Declare variables */
+	int i;
+	float sum1, sum2, diff;
 
 
-/* First sum */
-   sum1 = 0.0;
+	/* First sum */
+	sum1 = 0.0;
         /* incrising*/
-   for (i=1; i<=1000; i++) {
-        sum1 = sum1 + 1.0/i;
+	for (i=1; i<=1000; i++) {
+        	sum1 = sum1 + 1.0/i;
         /*another way would be sum1 += 1.01/i;*/
-   }
+	}
 
 
-/* Second sum */
-   sum2 = 0.0;
-/* decreasing*/
-   for (i=1000; i>0; i--) {
-        sum2 = sum2 + 1.0/i;
-   }
+	/* Second sum */
+	sum2 = 0.0;
+	/* decreasing*/
+	for (i=1000; i>0; i--) {
+        	sum2 = sum2 + 1.0/i;
+   	}
 
-   printf(" Sum1=%f\n",sum1);
-   printf(" Sum2=%f\n",sum2);
+	//Print out the results
+   	printf(" Sum1=%f\n",sum1);
+   	printf(" Sum2=%f\n",sum2);
 
-/* Find the difference */
-   diff = sum1 - sum2;
+	/* Find the difference */
+   	diff = sum1 - sum2;
 
-   printf(" Difference between the two is %f\n",diff);
+	//Print out the difference
+   	printf(" Difference between the two is %f\n",diff);
 
 }
 
--- practical02/conversion.c
+++ practical02/conversion.c
@@ -3,39 +3,37 @@
 
 int main(void) {
 
-/* Declare variables */
-   int i,inum,tmp,numdigits;
-   float fnum;
-   char binnum[60];
+	/* Declare variables */
+   	int i,inum,tmp,numdigits;
+   	float fnum;
+   	char binnum[60];
 
-/* Intialise 4-byte integer */
-   inum = 33554431;
-/* Convert to 4-byte float */
-   fnum = (float) inum;
-
-
-/* Convert to binary number (string)*/
-   i = 0; tmp = inum;
-   while (tmp > 0) {
-     sprintf(&binnum[i],"%1d",tmp%2);
-     tmp = tmp/2;
-     i++;
-   }
-
-/* Terminate the string */
-   binnum[i] = '\0';
+	/* Intialise 4-byte integer */
+   	inum = 33554431;
+	/* Convert to 4-byte float */
+   	fnum = (float) inum;
 
 
-/*Complite the expression*/
-/* Count the binary digits */
+	/* Convert to binary number (string)*/
+   	i = 0; tmp = inum;
+   	while (tmp > 0) {
+     		sprintf(&binnum[i],"%1d",tmp%2);
+     		tmp = tmp/2;
+     		i++;
+   	}
+
+	/* Terminate the string */
+  	 binnum[i] = '\0';
+
+
+	/*Complite the expression*/
+	/* Count the binary digits */
 	numdigits= ceil(log2f(inum));
 	printf("The number of digits is %d\n",numdigits);
 
 
 
-	printf("inum=%d,  fnum=%f, inum in binary=%s\n",
-      inum,fnum,binnum);
-
+	printf("inum=%d,  fnum=%f, inum in binary=%s\n", inum,fnum,binnum);
 
 }
 
