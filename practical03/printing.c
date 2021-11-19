32a91e3b1a1dc8ff6cf02b64e462561ecb458e62
U-TYNDALL\ana.ferreira <ana.ferreira@LAPCC483A621D3D.tyndall.ie>
2021-11-19 11:45:06 +0000

    Correct identation

--- practical03/printing.c
+++ practical03/printing.c
@@ -9,8 +9,7 @@ int main(void){
 	float fl1=1.11, fl2=2.2222;
 
 	//print the floats (floats with 4 decimal places)
-	printf("Two integers are %d %d and two floats are %.4f %.4f \n", i1, i2,
-fl1, fl2);
+	printf("Two integers are %d %d and two floats are %.4f %.4f \n", i1, i2, fl1, fl2);
 
 	return 0;
 }
